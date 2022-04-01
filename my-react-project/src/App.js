b9b2ee8d3651bbed5718492b874a080ceca98421
Abhishek-S-B <abhisheksb9740@gmail.com>
2022-04-02 01:36:22 +0530

    making changes in vtgbranc

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1></h1>
-     <p>nani monoda</p>
-     <h3>lenord</h3>
+     <h1>abhibranch chages</h1>
     </div>
   );
 }
